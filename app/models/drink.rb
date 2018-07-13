e179eb7fc89717048c0d57dff0f8b29bf9b03af7
Saul Ackerman <saulack@gmail.com>
2018-07-13 17:09:11 -0400

    Fix storage.yml

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -26,12 +26,12 @@ class Drink < ApplicationRecord
 
   validate :ensure_photo
 
-  # has_one_attached :photo
+  has_one_attached :photo
 
-  # def ensure_photo
-  #   unless self.photo.attached?
-  #     errors[:photo] << "must be attached"
-  #   end
-  # end
+  def ensure_photo
+    unless self.photo.attached?
+      errors[:photo] << "must be attached"
+    end
+  end
 
 end
